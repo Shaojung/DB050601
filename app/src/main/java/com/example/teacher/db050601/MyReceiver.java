cb2e14bd5f23d1e2a47552bb61488b06e85f9c65
Shaojung <shaojung@gmail.com>
2017-05-06 13:32:25 +0800

    Broadcast Receiver send notification

--- app/src/main/java/com/example/teacher/db050601/MyReceiver.java
+++ app/src/main/java/com/example/teacher/db050601/MyReceiver.java
@@ -1,9 +1,13 @@
 package com.example.teacher.db050601;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class MyReceiver extends BroadcastReceiver {
 
@@ -21,6 +25,13 @@ public class MyReceiver extends BroadcastReceiver {
         if (intent.getAction().equals("DB0506REC"))
         {
             Log.d("REC1", "onReceive!! click!!");
+            NotificationManager manager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+            Notification.Builder builder = new Notification.Builder(context.getApplicationContext());
+            builder.setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle("收到廣播")
+                    .setContentText("收到廣播");
+            Notification notification = builder.build();
+            manager.notify(3122, notification);
         }
     }
 }
