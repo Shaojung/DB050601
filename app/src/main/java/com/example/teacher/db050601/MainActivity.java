7e9f0810b8fef3a563a2baf2b4023ee3519e8896
Shaojung <shaojung@gmail.com>
2017-05-06 11:33:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/teacher/db050601/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.teacher.db050601;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click1(View v)
+    {
+        Intent it = new Intent();
+        it.setAction("DB0506REC");
+        sendBroadcast(it);
+    }
+}
